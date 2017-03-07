61b467542a55b2dd727dd57b82cfbef8c022afae
Reto Kaufmann <reto.kaufmann@adon.li>
2017-03-07 18:25:46 +0100

    .

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,9 +1,16 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * Created by rk on 07.03.17.
  */
 public class Products extends Controller{
+
+    // das NEUE PLAY Framework verlangt KEINE STATISCHEN METHODEN, ergibt sonst Fehler!
+    public Result list() {
+        return ok();
+    }
+
 }
