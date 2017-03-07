5ebf0c640a3d46d2047e10517b09367b6c88df12
Reto Kaufmann <reto.kaufmann@adon.li>
2017-03-07 18:38:34 +0100

    .

---
+++ app/model/Product.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rk on 07.03.17.
+ */
+public class Product {
+
+
+    // Mocking Data (nur zu Testzwecken)
+    private static List<Product> products;
+    // statischer Initialisierer, somit gibt es diese Variable nur 1x, egal wieviele male man instanziert
+    static {
+        products = new ArrayList<Product>();
+        products.add(new Product("1111111111111","Paperclips", "description 1"));
+        products.add(new Product("2222222222222","Paperclips", "description 2"));
+        products.add(new Product("3333333333333","Paperclips", "description 3"));
+        products.add(new Product("4444444444444","Paperclips", "description 4"));
+        products.add(new Product("5555555555555","Paperclips", "description 5"));
+    }
+
+
+    public String ean;
+    public String name;
+    public String description;
+
+    public Product() {}
+    public Product(String ean, String name, String description) {
+        this.ean = ean;
+        this.name = name;
+        this.description = description;
+    }
+    public String toString() {
+        return String.format("%s - %s", ean, name);
+    }
+}
