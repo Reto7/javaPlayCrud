74d9f497564e09b861e75ef7263c3f2860f242a5
Reto Kaufmann <reto.kaufmann@adon.li>
2017-03-14 20:46:05 +0100

    Asynchron !

--- app/controllers/Reports.java
+++ app/controllers/Reports.java
@@ -1,66 +1,76 @@
 package controllers;
 
-import model.Report;
 import java.util.List;
-import java.util.concurrent.Callable;
-import play.*;
-//import play.libs.F.Promise;
-import play.libs.F.*;
-import play.mvc.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
+import javax.inject.Singleton;
+
+import model.Report;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
- * Created by user on 12.03.2017.
+ * Created by suesstnorma1 on 14.03.17.
  */
+@Singleton
 public class Reports extends Controller {
-    /*
-    Every action method must return a Result
-    In order to be asynchronous, we need to return a Promise of a Result (Promise<Result>) instead.
-    A promise is a commitment to do or not do something.
-    A Promise<Result> will eventually be redeemed (eingeloest) with a value of type Result.
-    By giving a Promise<Result> instead of a regular Result, we are able to compute the result without
-    blocking anything. Play will then serve this result as soon as the promise is redeemed.
-    */
 
-    /*
-        public static Result index() {
-            Promise<Report> promiseOfKPIReport =
-                    play.libs.Akka.future(
-                            new Callable<Report>() {
-                                public Report call() {
-                                    return intensiveKPIReport();
-                                }
-                            }
-                    );
-            Promise<Report> promiseOfETAReport =
-                    play.libs.Akka.future(
-                            new Callable<Report>() {
-                                public Report call() {
-                                    return intensiveETAReport();
-                                }
-                            }
-                    );
-            Promise<List<Report>> promises =
-                    Promise.waitAll(promiseOfKPIReport, promiseOfETAReport);
-            return async(
-                    promises.map(
-                            new Function<List<Report>, Result>() {
-                                public Result apply(List<Report> reports) {
-                                    return ok(report.render(reports));
-                                }
-                            }
-                    )
-            );
+    public CompletionStage<Result> listReports() {
+
+        //                hier liste                                                          hier liste
+        //                -------------                                                       ---------------
+        //CompletionStage<List<Report>> promiseOfReport = CompletableFuture.supplyAsync(() -> loadReports());
+
+        System.out.println("Before start");
+
+        // Promise/Versprechen und Aufruf
+        CompletionStage<List<Report>> promiseOfReport =
+                CompletableFuture.supplyAsync(() -> loadReports());
+
+        System.out.println("After start");
+
+        // Das Promise wird nach Ausfuehrung (z.B. nach n Sekunden) hier eingeloest
+        CompletionStage<Result> promiseOfResult  =
+                promiseOfReport.thenApply(r  -> ok(Json.toJson(r)));
+
+        System.out.println("fertig");
+
+        return promiseOfResult;
+    }
+
+
+//    public CompletionStage<Result> kpiReports() {
+//
+//        CompletionStage<Report> promiseOfKPIReport =
+//                CompletableFuture.supplyAsync(
+//                        () -> intensiveKPIReport())
+//                ;
+//
+//
+//        CompletionStage<Result> promiseOfResult  =
+//                promiseOfKPIReport.thenApply(
+//                r  -> ok(Json.toJson(r)));
+//
+//        return promiseOfResult;
+//    }
+
+    public static List<Report> loadReports() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    public static Report intensiveKPIReport() {
-        Report r = new Report("KPI report");
-        r.execute();
-        return r;
+        return Report.reportListe;
     }
-    public static Report intensiveETAReport() {
-        Report r = new Report("ETA report");
-        r.execute();
-        return r;
-    }
-    */
+
+
+
+
+
+
+
+
+
 }
--- app/model/Report.java
+++ app/model/Report.java
@@ -1,26 +1,55 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.Logger;
 
 /**
- * Created by user on 12.03.2017.
+ * Created by suesstnorma1 on 14.03.17.
  */
+
 public class Report {
-    String name;
+
+
+    // Mocking Data (nur zu Testzwecken)
+    // List ist ein Interface!
+    public static List<Report> reportListe;
+    // statischer Initialisierer, somit gibt es diese Variable nur 1x, egal wieviele male man instanziert
+    static {
+        reportListe = new ArrayList<Report>();
+        reportListe.add(new Report("Report 1"));
+        reportListe.add(new Report("Report 2"));
+        reportListe.add(new Report("Report 3"));
+        reportListe.add(new Report("Report 4"));
+    }
+
+
+
+    private String name;
+
     public Report(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void execute() {
         long start = System.currentTimeMillis();
         Logger.info("starting intensive " + name + " report at " + start);
         try {
-            // Simulate/Fake long long execution time ...
             Thread.sleep(5000);
         } catch(Exception e) {}
-        Logger.info("done with intensive " + name + " report ");
-        Logger.info("took "
-                + ((System.currentTimeMillis() - start) / 1000) + "s");
+        Logger.info("done with intensive " + name + " report at " + System.currentTimeMillis());
+        Logger.info("took " + ((System.currentTimeMillis() - start) / 1000) + "s");
     }
+
     public String toString() {
         return name;
     }
